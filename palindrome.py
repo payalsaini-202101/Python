b8ca156cef1d2f7824ad827349f00646e97d01eb
Payal saini <91257698+payalsaini-202101@users.noreply.github.com>
2022-08-19 21:38:09 +0530

    Add files via upload

---
+++ palindrome.py
@@ -0,0 +1,28 @@
+class palindrome:
+    def _init_(self,string):
+        self.str1=string
+        self.unique_set=set()
+    def substring(self):
+        i = 0
+        while i <len(self.str1):
+            j = len(self.str1) - 1
+            while (j>i):
+                if self.str1[i] == self.str1[j]:
+                    if self.is_palindrome(i,j):
+                        self.unique_set.add(self.str1[i:j+1])
+                        i = (i+j)//2
+                        break
+                j -= 1
+            i+=1
+    def is_palindrome(self,strt,end):
+        while(strt<=end):
+            if(self.str1[strt]==self.str1[end]):
+                strt+=1
+                end-=1
+            else:
+                return False
+        return True
+string="abcbadefeda"
+pldrome1=palindrome(string)
+pldrome1.substring()
+print(pldrome1.unique_set)
